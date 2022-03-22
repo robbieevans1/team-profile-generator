f324695c901fe6c938b123d2974c339cbe815022
robcabobb <robcabobb@gmail.com>
2022-03-22 12:56:38 -0400

    edited tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,7 +1,7 @@
 const Intern = require('../lib/Intern');
 const intern = new Intern('robbie', '1235', 'robbie@gmail.com', 'Richmond');
 
-test('test if we can get the constructor values for the engineer object', () => {
+test('test if we can create values for intern object', () => {
     expect(intern.name).toBe('robbie');
     expect(intern.id).toBe('1235');
     expect(intern.email).toBe('robbie@gmail.com');
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,7 @@
 const Manager = require('../lib/Manager');
 const manager = new Manager('robbie', '1234', 'robbie@gmail.com', '8046464666');
 
-test('test if we can get the constructor values for the manager object', () => {
+test('test if we can create constructor values for the manager object', () => {
     expect(manager.name).toBe('robbie');
     expect(manager.id).toBe('1234');
     expect(manager.email).toBe('robbie@gmail.com');
